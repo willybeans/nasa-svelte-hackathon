90648267188db768fb5db62ace169c0cbb83faaf
will wedmedyk <willwedmedyk@gmail.com>
2025-12-08 13:22:52 -0500

    broken demo

---
+++ src/routes/nasa/tables/tanstack-table/+page.svelte
@@ -0,0 +1,27 @@
+<!-- <script lang="ts">
+  import { createSvelteTable, getCoreRowModel, flexRender } from '@tanstack/svelte-table'
+  const data = [{ id: 1, name: 'Ada' }]
+  const columns = [{ accessorKey: 'name', header: 'Name' }]
+  const table = createSvelteTable({ data, columns, getCoreRowModel: getCoreRowModel() })
+</script>
+
+<table>
+  <thead>
+    {#each table.getHeaderGroups() as hg}
+      <tr>
+        {#each hg.headers as header}
+          <th>{flexRender(header.column.columnDef.header, header.getContext())}</th>
+        {/each}
+      </tr>
+    {/each}
+  </thead>
+  <tbody>
+    {#each table.getRowModel().rows as row}
+      <tr>
+        {#each row.getVisibleCells() as cell}
+          <td>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
+        {/each}
+      </tr>
+    {/each}
+  </tbody>
+</table> -->
